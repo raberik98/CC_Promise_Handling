d4e2e8629369c0617cee0a7ed22af372d98cee5e
Erik <erik.raboczki98@gmail.com>
2023-01-24 21:45:40 +0100

    Added missing explanation.

--- ThenAndCatch.js
+++ ThenAndCatch.js
@@ -1,6 +1,11 @@
 //Here we will simulate how we can handle a promise or multiple promises with a fake fetch function.
 //IMPORTANT! Please read the README.md file first.
 
+
+//First let's understand what happens here. As a normal fetch would do, this function returns a promise.
+//And in that promise it returns some data as well, or if it's rejected, as a fetch would when it is unsuccessful it will return an error message.
+//We can manipulate this fake fetch function's outcome by passing an even number as argument when we want it to succeed,
+//or we can pass it an odd number when we want it to fail.
 const fakeFetchData = (number) => {
   const data = {
         name: "Professor",
